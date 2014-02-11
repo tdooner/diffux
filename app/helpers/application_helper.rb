b0fa59bb292f35647b28e8704a52daad33a315d2
Tom Dooner <tom.dooner@causes.com>
2014-02-10 22:57:02 -0800

    Add basic user display in navbar header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,11 @@ module ApplicationHelper
   def simplified_url(url)
     url.gsub(%r[(?:\Ahttp://|/\Z)], '')
   end
+
+  # @return [User] the logged in user, or nil
+  def current_user
+    return unless session[:user_id]
+
+    @logged_in_user ||= User.find(session[:user_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,10 @@ Diffux::Application.routes.draw do
     end
   end
 
+  # n.b.: this route is for the helper only, as any request to it is intercepted
+  # by Omniauth middleware before Rails gets it:
+  get 'auth/google_oauth2', as: :google_oauth2
+
   get 'auth/google_oauth2/callback', to: 'user_auth_callbacks#google_oauth2'
 
   root to: 'projects#index'
